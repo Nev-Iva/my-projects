5cf0f51aee554665f6f9794d22d5fba9aab5c4a5
Ivan <i.nevzorov@yandex.ru>
2020-09-01 01:14:34 +0300

    pylibs for DS hw4 and coursework added

--- basic python (0 level)/7 exception handling/1.py
+++
@@ -1,7 +0,0 @@
-fruit_list_1 = ['Яблоко', 'Банан', 'Киви', 'Апельсин', 'Манго', 'Персик']
-fruit_list_2 = ['Яблоко', 'Мандарин', 'Апельсин', 'Фрукт_1', 'Фрукт_2']
-
-result = []
-
-result = [fruit for fruit in fruit_list_1 if fruit in fruit_list_2]
-print(result)
